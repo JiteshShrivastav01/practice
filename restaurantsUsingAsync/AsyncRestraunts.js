70748c8f0f9a8728f481ab6a0177ae392dcce38a
JiteshShrivastav01 <106865324+JiteshShrivastav01@users.noreply.github.com>
2023-04-06 19:31:34 +0530

    async function

---
+++ restaurantsUsingAsync/AsyncRestraunts.js
@@ -0,0 +1,80 @@
+
+
+async function savetoCrudCrud(event){
+    event.preventDefault()
+    const dish = document.getElementById('dish').value
+    const price= document.getElementById('price').value
+    const table= document.getElementById('table').value
+   
+
+    const order={dish,price,table}
+
+    try{
+    const res= await axios.post('https://crudcrud.com/api/b985a7ed9ce74a499b318f5d4f32eaa1/asyncres',order)
+    onscreen(order);
+    }
+    catch(err){document.body.innerHTML=document.body.innerHTML + `<h4>Something Went Wrong in post</h4>`}
+   
+}
+
+
+window.addEventListener('DOMContentLoaded',async ()=>{
+    try{
+    const res=await axios.get('https://crudcrud.com/api/b985a7ed9ce74a499b318f5d4f32eaa1/asyncres')
+        for(var i=0;i<res.data.length;i++){
+            onscreen(res.data[i])
+    }
+    }
+    catch(err){document.body.innerHTML=document.body.innerHTML + `<h4>Something Went Wrong in window</h4>`}
+})
+
+
+function onscreen(order){
+    document.getElementById('dish').value=''
+    document.getElementById('price').value=''
+    
+    const parent1=document.getElementById('table1')
+    const parent2=document.getElementById('table2')
+    const parent3=document.getElementById('table3')
+    
+    const child=`<li id=${order._id}>${order.table} ---> ${order.dish} ---> ${order.price}
+    
+                <button id='btn2' onclick=Delete('${order._id}','${order.table}')>Delete</button></li>`
+
+    if(order.table ==='table1'){
+        parent1.innerHTML = parent1.innerHTML + child
+    }
+    else if(order.table ==='table2'){
+        parent2.innerHTML = parent2.innerHTML + child
+    }
+    else if(order.table ==='table3'){
+        parent3.innerHTML = parent3.innerHTML + child
+    }
+} 
+
+async function Delete(id,table){
+    try{
+    const data= await axios.delete(`https://crudcrud.com/api/b985a7ed9ce74a499b318f5d4f32eaa1/asyncres/${id}`)
+           removeFromScreen(id,table)
+    }
+    catch(err){document.body.innerHTML=document.body.innerHTML + `<h4>Something Went Wrong</h4>`}
+    }
+
+function removeFromScreen(id,table){
+    const parent1=document.getElementById('table1')
+    const parent2=document.getElementById('table2')
+    const parent3=document.getElementById('table3')
+    const child=document.getElementById(id)
+
+    if(child){
+        if(table==='table1'){
+            parent1.removeChild(child)
+        }
+        if(table==='table2'){
+            parent2.removeChild(child)
+        }
+        if(table==='table3'){
+            parent3.removeChild(child)
+        }
+    }
+}
